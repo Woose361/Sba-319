d9efbc4742498bba3d6537da7c834ad991c4ebd1
woose361 <devon.owusu361@gmail.com>
2025-01-16 16:10:42 -0500

    3rd commit

--- db/conn.js
+++ db/conn.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+        mongoose.connect(process.env.MONGO_URI);
+        mongoose.connection.once("open", () => {
+          console.log("connected to mongodb");
         });
         console.log('Successfully connected to MongoDB');
     } catch (error) {
